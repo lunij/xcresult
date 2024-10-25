6a0d681e221db60d5813979ef4eb8f7182745f45
Marc S <lunij@users.noreply.github.com>
2024-10-25 15:38:15 +0200

    Truncate to byte limit (#19)

--- src/output.ts
+++ src/output.ts
@@ -1,36 +1,26 @@
 import * as core from '@actions/core'
 import { Annotation } from './report.js'
 
+const byteLimit = 65535
+const annotationLimit = 50
+
 export function createOutput({
   title,
   summary,
-  annotations,
-  characterLimit = 65535,
-  annotationLimit = 50
+  annotations
 }: {
   title: string
   summary: string
   annotations: Annotation[]
-  characterLimit?: number
-  annotationLimit?: number
 }) {
-  if (title.length > characterLimit) {
-    title = title.slice(0, characterLimit)
-    core.warning(
-      `The title is truncated because the character limit of ${characterLimit} was exceeded.`
-    )
-  }
-
-  if (summary.length > characterLimit) {
-    summary = summary.slice(0, characterLimit)
-    core.warning(
-      `The summary is truncated because the character limit of ${characterLimit} was exceeded.`
-    )
-  }
+  title = truncateToMaxBytes(title, 'title', byteLimit)
+  summary = truncateToMaxBytes(summary, 'summary', byteLimit)
 
   if (annotations.length > annotationLimit) {
+    core.warning(
+      `Annotations are truncated because the limit of ${annotationLimit} was exceeded by ${annotations.length - annotationLimit} annotations.`
+    )
     annotations = annotations.slice(0, annotationLimit)
-    core.warning(`Annotations are truncated because the limit of ${annotationLimit} was exceeded.`)
   }
 
   return {
@@ -38,4 +28,29 @@ export function createOutput({
     summary,
     annotations
   }
+}
+
+function truncateToMaxBytes(str: string, name: string, maxBytes: number): string {
+  const encoder = new TextEncoder()
+  const decoder = new TextDecoder()
+
+  let byteArray = encoder.encode(str)
+
+  if (byteArray.length <= maxBytes) {
+    return str
+  }
+
+  core.warning(
+    `The ${name} is truncated because the byte limit of ${maxBytes} was exceeded by ${byteArray.length - maxBytes} bytes.`
+  )
+
+  byteArray = byteArray.slice(0, maxBytes)
+
+  let truncatedStr = decoder.decode(byteArray)
+
+  while (encoder.encode(truncatedStr).length > maxBytes) {
+    truncatedStr = truncatedStr.slice(0, -1)
+  }
+
+  return truncatedStr
 }
